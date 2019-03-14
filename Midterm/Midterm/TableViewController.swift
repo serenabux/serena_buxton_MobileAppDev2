91419d20cad5e56aa14357c75b91814a46029160
Serena Buxton <serena.buxton@colorado.edu>
2019-03-14 12:39:33 -0600

    Midterm
    Delete working

--- Midterm/Midterm/TableViewController.swift
+++ Midterm/Midterm/TableViewController.swift
@@ -48,25 +48,29 @@ class TableViewController: UITableViewController {
     
 
     
-    /*
+
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+ 
 
-    /*
+
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            //Delete the country from the array
+            movies.remove(at: indexPath.row)
+            //Delete from the data model instance
+           movieData.deleteMovie(index: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
